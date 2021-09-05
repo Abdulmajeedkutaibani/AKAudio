adbe1c53bb91981dd959212ae056d7713a85d537
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-09-05 06:57:49 +0300

    add circle pattern to speaker1

--- src/components/Home/Speaker1.js
+++ src/components/Home/Speaker1.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import speaker1 from '../../photoes/home/desktop/image-speaker-zx9.png';
+import circles from '../../photoes/home/desktop/pattern-circles.svg';
 import Btn1 from '../Buttons/Btn1';
 import ProductBrief from '../Products/ProductBrief';
 
 const Speaker1 = () => {
   return (
     <div className='speaker1 w-full bg-orangy overflow-hidden relative rounded-md bg-circles'>
-      <div className='container w-5/6 h-xl flex md:flex-row justify-between items-center overflow-hidden'>
-        <img src={speaker1} alt='' className='h-3/4 absolute -bottom-md' />
+      <div className='container w-5/6 h-xl flex md:flex-row justify-between items-center  '>
+        <div className='image-container h-full w-full  flex justify-center'>
+          <img
+            src={speaker1}
+            alt=''
+            className='h-3/4 absolute -bottom-md z-20'
+          />
+          <img src={circles} alt='' className='absolute z-10 w-3/4' />
+        </div>
         {/* ////////////////////// */}
         <div className={`flex-col w-1/2 space-y-lg ml-auto `}>
           <h1 className=' text-whity text-xxxl font-bold  tracking-wider w-1/2'>
