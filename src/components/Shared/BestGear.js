1bbb52c7c25de4e537a186cf015601fd552a8e96
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-09-21 19:29:10 +0300

    Update BestGear.js

--- src/components/Shared/BestGear.js
+++ src/components/Shared/BestGear.js
@@ -6,12 +6,12 @@ import bestGearMobile from '../../photoes/shared/mobile/image-best-gear.jpg';
 const BestGear = () => {
   return (
     <div
-      className='container w-full  flex flex-col-reverse lg:flex-row justify-between items-center mt-48 mb-48 md:mb-24 md:mt-24'
+      className='container w-full  flex flex-col-reverse lg:flex-row justify-between items-center mb-30 mt-30 lg:mt-48 lg:mb-48 md:mb-24 md:mt-24'
       data-aos='fade-in'
       data-aos-duration='900'
     >
-      <div className=' lg:w-1/3 w-4/5 text-center lg:text-left '>
-        <h2 className='text-xxl mb-8 font-bold uppercase leading-11'>
+      <div className=' lg:w-1/3 w-full md:w-4/5 text-center lg:text-left '>
+        <h2 className=' text-larg md:text-xxl mb-8 font-bold uppercase leading-11'>
           Bringing you the <span className='text-orangy'>best</span> audio gear
         </h2>
         <p className='text-xs opacity-50 font-thin leading-6'>
